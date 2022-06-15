b93f1453d42402fc57cf97467554fdb431ff429c
Aditya Pawar <adityapawar743.ap5@gmail.com>
2022-06-16 01:03:07 +0530

    Created first custom component inside custom-component project.

---
+++ custom-component/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CustomComponent from './components/CustomComponent/CustomComponent';
+
+function App() {
+  return (
+    <div className="App">
+        <CustomComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ custom-component/src/components/CustomComponent/CustomComponent.js
@@ -0,0 +1,11 @@
+import './CustomComponent.css';
+
+function CustomComponent() {
+    return(
+        <div>
+            <h1 className='heading'>FIRST CUSTOM COMPONENT</h1>
+        </div>
+    );
+}
+
+export default CustomComponent;
